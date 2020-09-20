53a5c07c49a311ff4fd91c6d9c2f52c5d8187001
founta <andrewfountain1@gmail.com>
2020-09-20 17:10:36 -0400

    Set map hostility right away. Change hideout reversion on abandon
    Hideout now correctly reverts to the original bandit clan
    Also now gives a notification when you take the hideout (meh)

--- TakeHideouts/TakeHideouts.cs
+++ TakeHideouts/TakeHideouts.cs
@@ -10,6 +10,8 @@ using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors.AiBehaviors;
+using SandBox.View.Map;
 
 using System.Xml;
 
@@ -36,7 +38,7 @@ namespace TakeHideouts
         }
         if (!factionSet)
         {
-          __result = (IFaction)Clan.BanditFactions.FirstOrDefault<Clan>(); //sets it to mountain bandits if no parties
+          __result = (IFaction)Clan.BanditFactions.FirstOrDefault<Clan>(); //sets it to some default bandit clan if no parties
         }
       }
     }
@@ -74,7 +76,14 @@ namespace TakeHideouts
     {
       return;
     }
-
+    /*
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(PartyAi), "SetDefaultBehavior")]
+    public static void SetDefaultBehavior(PartyAi instance, AiBehavior newAiState, PartyBase targetParty)
+    {
+      return;
+    }
+    */
   }
 
   public class TakeHideoutsMain : MBSubModuleBase
@@ -139,6 +148,9 @@ namespace TakeHideouts
     {
       ref Hideout hideout = ref Settlement.CurrentSettlement.Hideout;
 
+      //set our main hero as the new owener of the hideout (not needed)
+      //hideout.Settlement.Party.Owner = Hero.MainHero;
+
       //This appears to default false for hideouts and doesn't appear to change anything
       //for the hideouts. hopefully changing it doesn't break anything
       //It looks like it is used for when towns or castles get taken. Should be ok to re-use for hideouts
@@ -148,9 +160,19 @@ namespace TakeHideouts
       {
         if (party.IsBandit) //don't change the main party
         {
+          //InformationManager.DisplayMessage(new InformationMessage($"Clan leader {(party.ActualClan.Leader == null ? "null" : "not null")}"));
+          //InformationManager.DisplayMessage(new InformationMessage($"Leader {party.ActualClan.Leader.Name.ToString()}"));
+
           party.ActualClan = Hero.MainHero.Clan; //convert bandits in the hideout to our cause
           //party.Party.Owner = Hero.MainHero; //this makes it so that you can see them in the clan menu
-          //party.HomeSettlement = hideout.Settlement; //likely already set to this, doesn't seem to do anything
+          party.HomeSettlement = hideout.Settlement; //likely already set to this, doesn't seem to do anything
+
+          //Tell everyone except the bandit boss to patrol around the hideout
+          if (!party.IsBanditBossParty)
+          {
+            //party.SetMoveDefendSettlement(party.HomeSettlement);
+            party.SetMovePatrolAroundSettlement(party.HomeSettlement);
+          }
 
           //remove from war party list so that these bandits don't use up party slots
           ExposeInternals.RemoveWarPartyInternal(party.ActualClan, party);
@@ -161,6 +183,14 @@ namespace TakeHideouts
       //re-open hideout menu
       //actually closes the game menu but I don't make the main hero leave the settlement so it just re-opens
       Campaign.Current.GameMenuManager.ExitToLast(); //re-opens hideout menu
+
+      //found this in one of the DLLs should update map color?
+      //doesnt really do anything
+      //hideout.Settlement.Party.Visuals.SetMapIconAsDirty();
+
+      //update hideout's appearance on map. Also gives a notification that you're the new owner
+      ChangeOwnerOfSettlementAction.ApplyByBarter(Hero.MainHero, hideout.Settlement);
+
       return;
     }
 
@@ -176,16 +206,31 @@ namespace TakeHideouts
       ref Hideout hideout = ref Settlement.CurrentSettlement.Hideout;
       hideout.IsTaken = false;
 
+      //get correct original bandit clan
+      Clan originalBanditClan = Clan.BanditFactions.FirstOrDefault<Clan>();
+      foreach (Clan banditClan in Clan.BanditFactions)
+      {
+        if (banditClan.Culture == hideout.Settlement.Culture)
+        {
+          originalBanditClan = banditClan;
+          break;
+        }
+      }
+
+      //set each party clan back to the correct original bandit clan 
       foreach (MobileParty party in hideout.Settlement.Parties)
       {
         //don't set the main party's clan. Holy crap how did this not cause problems
         if (party != MobileParty.MainParty)
         {
-          //sets the party clan to some default bandit clan
-          //appears to be kind of random. sometimes forest bandits, sometimes steppe bandits
-          party.ActualClan = Clan.BanditFactions.FirstOrDefault<Clan>();
+          party.ActualClan = originalBanditClan;
         }
       }
+
+      //remove ownership of hideout
+      //by setting it to the hero of that bandit clan
+      //hopefully doesn't blow anything up (doesn't appear to)
+      ChangeOwnerOfSettlementAction.ApplyByDefault(originalBanditClan.Leader, hideout.Settlement);
 
       //re-opens hideout menu
       Campaign.Current.GameMenuManager.ExitToLast();
@@ -217,6 +262,7 @@ namespace TakeHideouts
       ref Hideout hideout = ref Settlement.CurrentSettlement.Hideout;
 
       //Allows putting troops in the settlement's party object. Appears to persist across saving/loading
+      //can also see the prisoners you stick in the hideout here. that's fine
       PartyScreenManager.OpenScreenAsLoot(hideout.Settlement.Party);
       return;
     }
